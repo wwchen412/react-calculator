087c51bd52ae316bd38103bc8014ea041d296f21
william <wwchen412@gmail.com>
2020-07-10 12:22:42 +0800

    Updates

---
+++ precache-manifest.cde5c8cb0c059c48ef6a3a03ec32225b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fb1e49e8d1eb38470e1c0421897a27",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "4a21f35b4e7d241db0fc",
+    "url": "/react-calculator/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "e2dc58a0432dc7240ba0",
+    "url": "/react-calculator/static/js/2.a3ed1d9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-calculator/static/js/2.a3ed1d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a21f35b4e7d241db0fc",
+    "url": "/react-calculator/static/js/main.33558b5e.chunk.js"
+  },
+  {
+    "revision": "14453d16db6c0e44d6da",
+    "url": "/react-calculator/static/js/runtime-main.7b47f775.js"
+  }
+]);
